ecef778fe9019eb5dab7768ae696fc84a74ac9f8
olamide jubril <olamidejubril68@gmail.com>
2023-05-19 05:41:56 +0100

    Initial commit

---
+++ src/store/rootReducer.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux'
+import theme from './slices/themeSlice'
+import auth from './slices/authSlice'
+
+const rootReducer = (asyncReducers) => (state, action) => {
+    const combinedReducer = combineReducers({
+        theme,
+        auth,
+        ...asyncReducers,
+    })
+    
+    return combinedReducer(state, action)
+}
+  
+export default rootReducer;
